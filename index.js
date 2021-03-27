d206900f74dbed66c5840823486a592120520ff3
emilien.wacrenier <emilien.wacrenier@atos.net>
2021-03-27 14:30:13 +0100

    finishing setup of routing

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+/* eslint-disable new-cap */
 const express = require('express');
+
+const router = require('./src/routage/route.js');
+const router2 = require('./src/routage/route2');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, you are on \'/\'');
-});
-
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
 });
+
+app.use('/route', router);
+app.use('/route2', router2);
+
+module.exports = app;
