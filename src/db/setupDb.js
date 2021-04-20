948684208d2f2a8da8216067641184835e01be8a
emilien wacrenier <wacrenieremilien@gmail.com>
2021-04-20 17:03:14 +0200

    feat/ connecting with mongoose and creation of schema et methods in setupDb.js

--- src/db/setupDb.js
+++ src/db/setupDb.js
@@ -1,22 +1,33 @@
-const mongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
 const logger = require('../utils/winstonConfig');
 
-const dbUrl = 'mongodb://localhost:27017';
+const dbUrl = 'mongodb://localhost:27017/nodejs_cheatSheet';
 
-const dbName = 'nodejs_cheatSheet';
-const client = mongoClient(dbUrl);
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const dbConnection = client.connect(function(err) {
-    assert.equal(null, err);
+const db = mongoose.connection;
+db.on('error', () => logger.error('Connection error'));
+db.once('open', function() {
     logger.info('Connected to server');
 
-    const db = client.db(dbName);
+    const r1m1Schema = new mongoose.Schema({
+        prp1: String,
+        prp2: Boolean,
+    });
+    r1m1Schema.methods.methode1 = function() {
+        logger.info('prp1: ' + this.prp1 + ' prp2: ' + this.prp2);
+    };
+    const r1m1 = mongoose.model('r1m1', r1m1Schema);
 
-    client.close();
+    const oneR1m1 = new r1m1({prp1: 'nameForOneR1m1', prp2: true});
+
+    oneR1m1.save(function(err, oneR1m1) {
+        if (err) return logger.error(err);
+        oneR1m1.methode1();
+    });
 });
 
 module.exports = {
-    dbConnection,
+    mongoose,
 };
